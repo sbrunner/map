81123c64c79d7c01bb85fd29b3c5019a98d167cf
Stéphane Brunner <stephane.brunner@camptocamp.com>
2010-11-09 20:49:28 +0100

    remove old mapfish

--- MapFishApp/mapfishapp/tests/functional/test_cyclerouting.py
+++
@@ -1,6 +0,0 @@
-from mapfishapp.tests import *
-
-class TestCycleroutingController(TestController):
-    def test_index(self):
-        response = self.app.get(url(controller='cyclerouting'))
-        # Test response...
--- MapFishApp/mapfishapp/tests/functional/test_cyclerouting2.py
+++
@@ -1,6 +0,0 @@
-from mapfishapp.tests import *
-
-class TestCyclerouting2Controller(TestController):
-    def test_index(self):
-        response = self.app.get(url(controller='cyclerouting2'))
-        # Test response...
